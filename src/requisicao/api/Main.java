41910310132895a733fc3d709ad3b575c0a3cd29
Edu Liposcki <liposcki@outlook.com>
2022-11-17 14:34:00 -0300

    Criando requisição dos filmes.

---
+++ src/requisicao/api/Main.java
@@ -0,0 +1,29 @@
+package requisicao.api;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import gitignore.Config;
+
+public class Main {
+
+	public static void main(String[] args) throws URISyntaxException {
+
+		Config apikey = new Config();
+		
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest
+			.newBuilder()
+			.uri(new URI("https://imdb-api.com/en/API/Top250Movies/" + apikey.getApikey()))
+			.GET()
+			.build();
+		
+		client.sendAsync(request, BodyHandlers.ofString())
+			.thenApply(HttpResponse::body)
+			.thenAccept(System.out::println)
+			.join();
+	}
+}	
