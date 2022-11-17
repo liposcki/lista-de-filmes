cd98bdf1a041c4c4944f86f3aece71f80adb1457
Edu Liposcki <liposcki@outlook.com>
2022-11-17 19:57:03 -0300

    Alteração da API Key para variável de ambiente.

--- src/requisicao/api/Main.java
+++ src/requisicao/api/Main.java
@@ -6,18 +6,14 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
-import gitignore.Config;
-
 public class Main {
 
 	public static void main(String[] args) throws URISyntaxException {
-
-		Config config = new Config();
 		
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest
 			.newBuilder()
-			.uri(new URI("https://imdb-api.com/en/API/Top250Movies/" + config.getApikey()))
+			.uri(new URI("https://imdb-api.com/en/API/Top250Movies/" + System.getenv("ApiKeyIMDB")))
 			.GET()
 			.build();
 		
@@ -25,5 +21,6 @@ public class Main {
 			.thenApply(HttpResponse::body)
 			.thenAccept(System.out::println)
 			.join();
+	
 	}
-}	
+}
