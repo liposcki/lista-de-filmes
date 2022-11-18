20ef387749168268702482281ce94547848b1ca9
Edu Liposcki <liposcki@outlook.com>
2022-11-18 11:49:25 -0300

    Aplicando boas práticas (API key e URI) e implementando o retorno em uma variável.

--- src/requisicao/api/Main.java
+++ src/requisicao/api/Main.java
@@ -1,6 +1,6 @@
 package requisicao.api;
+
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -8,19 +8,21 @@ import java.net.http.HttpResponse.BodyHandlers;
 
 public class Main {
 
-	public static void main(String[] args) throws URISyntaxException {
+	public static void main(String[] args) {
+		
+		String apiKeyIMDB = System.getenv("ApiKeyIMDB");
+		URI apiIMDB = URI.create("https://imdb-api.com/en/API/Top250Movies/" + apiKeyIMDB);
 		
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest
 			.newBuilder()
-			.uri(new URI("https://imdb-api.com/en/API/Top250Movies/" + System.getenv("ApiKeyIMDB")))
-			.GET()
+			.uri(apiIMDB)
 			.build();
 		
-		client.sendAsync(request, BodyHandlers.ofString())
+		String json = client.sendAsync(request, BodyHandlers.ofString())
 			.thenApply(HttpResponse::body)
-			.thenAccept(System.out::println)
 			.join();
 	
+		System.out.println(json);
 	}
 }
